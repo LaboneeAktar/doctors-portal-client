4beb9f18be48fa543ba7c45b9ee979d19f0adf95
laboneeaktar <labonipub@gmail.com>
2022-11-20 14:57:36 +0600

    Add Payment System

---
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -0,0 +1,67 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const CheckoutForm = () => {
+  const [cardError, setCardError] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log("[PaymentMethod]", paymentMethod);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-outline btn-primary btn-sm mt-5 font-normal"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-600">{cardError}</p>
+    </div>
+  );
+};
+
+export default CheckoutForm;
